101abb4d0cd761ddf5f5252b9906ac8cfa720104
Pedro Sudré <pedrosudre04@gmail.com>
2023-03-28 22:22:27 -0300

    atualização classe pessoa

--- src/main/java/com/example/scfapi/model/entity/Pessoa.java
+++ src/main/java/com/example/scfapi/model/entity/Pessoa.java
@@ -5,6 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.util.Date;
 
 
